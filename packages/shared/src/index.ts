13722ce91d5670b25ae51bdc625bed680bc0ca56
liberacaoconnectpro <liberacaoconnectpro@gmail.com>
2026-05-14 13:34:35 -0300

    refactor: Append file extensions to imports
    Adds `.ts` and `.tsx` extensions to module imports in shared and fisio packages. This improves module resolution and adheres to best practices in newer TypeScript versions.

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,4 +1,4 @@
-export * from './lib/firebase';
-export * from './lib/desktop-utils';
-export * from './services/dbService';
+export * from './lib/firebase.ts';
+export * from './lib/desktop-utils.ts';
+export * from './services/dbService.ts';
 // As other components are exported, we add them here.
